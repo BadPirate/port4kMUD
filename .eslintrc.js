dcc13f9400133adea5e90180139c54152a67e6b3
BadPirate <badpirate@gmail.com>
2025-03-28 16:21:19 -0700

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['next/core-web-vitals', 'airbnb', 'airbnb/hooks'],
+  extends: ['next/core-web-vitals', 'airbnb', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -29,6 +29,6 @@ module.exports = {
       unnamedComponents: 'arrow-function',
     }],
     'react/react-in-jsx-scope': 0,
-    'import/extensions': ['error', 'always', { tsx: 'never', js: 'never' }],
+    'import/extensions': ['error', 'always', { tsx: 'never', js: 'never', ts: 'never' }],
   },
 }
